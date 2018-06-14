a723aa52dee964723edd7ec5bc465ed0d2413d14
Al Bassett <albassett@Als-MacBook-Pro.local>
2018-06-13 22:47:04 -0500

    working locally, need to deploy

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Pikachu',
+		photo: 'http://thaumcraft-3.wikia.com/wiki/File:Pikachu.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Bruno',
+		photo: 'http://pokemon.wikia.com/wiki/File:Bruno_anime.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+    },
+    {
+		name: 'Machamp',
+		photo: 'http://gaming.wikia.com/wiki/File:Machamp.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Lorelie',
+		photo: 'http://pokemon.wikia.com/wiki/File:Lorelei_Pokemon.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Snorlax',
+		photo: 'http://pokepasta.wikia.com/wiki/File:Snorlax_2.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Sabrina',
+		photo: 'http://images2.fanpop.com/images/photos/7600000/Psychic-Sabrina-gym-leader-pokemon-7661544-300-400.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Blaine',
+		photo: 'https://vignette.wikia.nocookie.net/pokemon/images/8/81/HGSS_Blaine.png/revision/latest?cb=20160123180716',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Lt. Surge',
+		photo: 'https://vignette.wikia.nocookie.net/parody/images/9/95/L.T._Surge.png/revision/latest?cb=20160823160422',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Giovanni',
+		photo: 'https://pre00.deviantart.net/2efb/th/pre/i/2014/229/0/7/giovanni_redesign_by_jaeon009-d7vm2hp.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Erica',
+		photo: 'https://static.zerochan.net/Erika.%28Pok%C3%A9mon%29.full.487526.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
